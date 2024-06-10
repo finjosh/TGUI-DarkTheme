d51adab3d87aa07607337727c4cfab29cef45e2e
Joshua <joshuafinemore@gmail.com>
2024-06-10 16:07:29 -0400

    Added a "dynamic" widget display     - shows all widgets and does extra for some     - any new widgets will be added automatically but might not show all functionality Updated theme so that the panel uses the custom var "Padding" Updated padding to be 3 by default

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <filesystem>
 
 #include "SFML/Graphics.hpp"
 
 #include "TGUI/TGUI.hpp"
 #include "TGUI/Backend/SFML-Graphics.hpp"
+#include "TGUI/AllWidgets.hpp"
 
 using namespace std;
 using namespace sf;
@@ -96,6 +98,199 @@ void runExample(tgui::Gui& gui)
     gui.getWidgets().back()->setPosition(500,375);
 }
 
+void makeAllWidgets(tgui::Gui& gui)
+{
+    auto widgetHolder = tgui::HorizontalWrap::create();
+    auto scroll = tgui::ScrollablePanel::create();
+    scroll->add(widgetHolder);
+    widgetHolder->getRenderer()->setSpaceBetweenWidgets(10);
+
+    for (auto widgetName: tgui::WidgetFactory::getWidgetTypes())
+    {
+        if (widgetName == "RadioButtonGroup") // just look at the radio button
+            continue;
+
+        auto panel = tgui::Panel::create();
+        auto label = tgui::Label::create(widgetName);
+        label->setHorizontalAlignment(tgui::Label::HorizontalAlignment::Center);
+        label->setVerticalAlignment(tgui::Label::VerticalAlignment::Center);
+        panel->add(label);
+        float temp = panel->getWidgets().back()->getSize().y;
+        panel->add(tgui::WidgetFactory::getConstructFunction(widgetName)());
+        auto lastWidget = panel->getWidgets().back();
+        lastWidget->setPosition(0, temp);
+        temp += lastWidget->getFullSize().y;
+        
+        if (widgetName == "ChatBox")
+        {
+            auto chat = lastWidget->cast<tgui::ChatBox>();
+            chat->addLine("Adding 500 random lines");
+            chat->setLineLimit(1000);
+            for (int i = 0; i < 500; i++)
+            {
+                tgui::String temp;
+                for (int i = 0; i < 10; i++)
+                {
+                    temp += char(rand()%25+65);
+                }
+                chat->addLine(temp);
+            }
+        }
+        else if (widgetName == "MessageBox")
+        {
+            auto msg = lastWidget->cast<tgui::MessageBox>();
+            msg->setText("Some random message for the message box");
+        }
+        else if (auto container = lastWidget->cast<tgui::Container>())
+        {
+            if (widgetName != "ColorPicker" && widgetName != "FileDialog")
+            for (auto file: std::filesystem::directory_iterator{"TestPhotos"})
+            {
+                auto picture = tgui::Picture::create(tgui::Texture{file.path().generic_string()});
+                picture->setSize({100, 100*(picture->getSize().y/picture->getSize().x)});
+                container->add(picture); // can have many overlapping photos but we also want to show if the container sorts the widgets
+                container->updateChildrenWithAutoLayout();
+                float width = 0;
+                float height = 0;
+                for (auto widget: container->getWidgets())
+                {
+                    if (widget->getFullSize().x > width)
+                        width = widget->getFullSize().x;
+                    float temp = widget->getPosition().y + widget->getFullSize().y;
+                    if (temp > height)
+                        height = temp;
+                }
+                container->setSize({width, height});
+            }
+        }
+        else if (auto button = lastWidget->cast<tgui::Button>())
+        {
+            button->setText(widgetName + " Text");
+            auto disabled = lastWidget->clone()->cast<tgui::Button>();
+            disabled->setPosition({0, temp});
+            disabled->setEnabled(false);
+            disabled->setText(widgetName + " Text (Disabled)");
+            panel->add(disabled);
+        }
+        else if (widgetName == "ProgressBar")
+        {
+            auto progress = lastWidget->cast<tgui::ProgressBar>();
+            progress->setMaximum(999);
+            progress->setValue(rand()%1000);
+        }
+        else if (widgetName == "ComboBox")
+        {
+            auto box = lastWidget->cast<tgui::ComboBox>();
+            box->addItem("Combo Box");
+            box->addItem("Combo Box1");
+            box->addItem("Combo Box2");
+            box->addItem("Combo Box3");
+            box->setSelectedItemByIndex(0);
+        }
+        else if (widgetName == "Label")
+        {
+            lastWidget->cast<tgui::Label>()->setText("Epic label");
+        }
+        else if (widgetName == "RichTextLabel")
+        {
+            lastWidget->cast<tgui::RichTextLabel>()->setMaximumTextWidth(200);
+            lastWidget->cast<tgui::RichTextLabel>()->setText("The RichTextLabel widget supports formatting text with <b>bold</b>, <i>italics</i>, <u>underlined</u> "
+                "and even <s>strikethrough</s>. Each letter can have a separate <size=15>size</size> or <color=blue>color</color>. "
+                "This allows for some <b><color=#ff0000>C</color><color=#ffbf00>O</color><color=#80ff00>L</color>"
+                "<color=#00ff40>O</color><color=#00ffff>R</color><color=#0040ff>F</color><color=#7f00ff>U</color>"
+                "<color=#ff00bf>L</color></b> text. Lines that are too long will wrap around and a vertical scrollbar can be included "
+                "when there are too many lines!");
+        }
+        else if (widgetName == "Picture")
+        {
+            for (auto file: std::filesystem::directory_iterator{"TestPhotos"})
+            {
+                lastWidget->cast<tgui::Picture>()->getRenderer()->setTexture({file.path().generic_string()});
+                if (rand()%2) // pick random breaking point
+                    break;
+            }
+        }
+        else if (widgetName == "SeparatorLine")
+        {
+            lastWidget->setHeight(25);
+            lastWidget->setWidth(5);
+            lastWidget->setPosition({"50%", lastWidget->getPosition().y});
+        }
+        else if (widgetName == "RangeSlider")
+        {
+            auto slider = lastWidget->cast<tgui::RangeSlider>();
+            slider->setMinimum(0);
+            slider->setMaximum(999);
+            slider->setSelectionStart(rand()%500);
+            slider->setSelectionEnd(rand()%500+500);
+        }
+        else if (widgetName == "ListBox")
+        {
+            auto list = lastWidget->cast<tgui::ListBox>();
+            list->addItem("Some Item");
+            list->addItem("Another Item");
+            list->addItem("I don't know what else to put");
+            list->addItem("Something");
+            list->addItem("Cat");
+        }
+        else if (widgetName == "ListView")
+        {
+            auto list = lastWidget->cast<tgui::ListView>();
+            list->setResizableColumns(true);
+            list->addColumn("Name");
+            list->addColumn("Value");
+            list->addColumn("Something Else");
+            list->addItem({"Cat", "100", "..."});
+            list->addItem({"a item", "13", "something"});
+            list->addItem({"something", "54", "..."});
+            list->addItem({"i dont know", "67", "who knows"});
+            list->addItem({"anything", "62", "..."});
+        }
+        else if (widgetName == "MenuBar")
+        {
+            auto bar = lastWidget->cast<tgui::MenuBar>();
+            bar->addMenu("Disabled");
+            bar->setMenuEnabled("Disabled", false);
+            bar->addMenu("Enabled");
+            bar->addMenuItem("Enabled", "Menu Item");
+            bar->setSize({200, 50});
+        }
+        else if (widgetName == "Tabs")
+        {
+            auto tabs = lastWidget->cast<tgui::Tabs>();
+            tabs->add("Some Tab");
+            tabs->add("Another Tab");
+            tabs->add("Cats!");
+            tabs->add("Yet Another Tab");
+        }
+        else if (widgetName == "TreeView")
+        {
+            auto tree = lastWidget->cast<tgui::TreeView>();
+            tree->addItem({"Root", "Branch", "Branch Branch", "Leaf"});
+            tree->addItem({"Cats", "more Cats?", "Even more cats", "so many cats"});
+            tree->addItem({"Why Cats?", "Cats are cool"});
+        }
+        
+        float width = 0;
+        float height = 0;
+        for (auto widget: panel->getWidgets())
+        {
+            if (widget->getFullSize().x > width)
+                width = widget->getFullSize().x;
+            height += widget->getFullSize().y;
+        }
+        panel->setSize({width + panel->getSharedRenderer()->getPadding().getLeft() + panel->getSharedRenderer()->getPadding().getRight() + 5, 
+                        height + panel->getSharedRenderer()->getPadding().getTop() + panel->getSharedRenderer()->getPadding().getBottom() + 5});
+
+        widgetHolder->add(panel);
+    }
+
+    widgetHolder->updateChildrenWithAutoLayout();
+    widgetHolder->setSize({"100%", widgetHolder->getWidgets().back()->getPosition().y + widgetHolder->getWidgets().back()->getFullSize().y + 15}); // have to get last widget because size does not update for some reason
+    scroll->setContentSize({scroll->getSize().x, widgetHolder->getFullSize().y}); 
+    gui.add(scroll);
+}
+
 int main()
 {
     // setup for sfml and tgui
@@ -108,7 +303,8 @@ int main()
     tgui::Theme::setDefault("themes/Dark.txt");
     // -----------------------
     
-    runExample(gui);
+    // runExample(gui);
+    makeAllWidgets(gui);
 
     while (window.isOpen())
     {
--- makefile
+++ makefile
@@ -1,123 +1,128 @@
 # directory that the makefile was called from
-mkfile_path:=$(abspath $(lastword $(MAKEFILE_LIST)))
-project_name:=$(strip $(notdir $(patsubst %/,%,$(dir $(mkfile_path)))))
-project_dir:=$(patsubst %/makefile,%, $(mkfile_path))
+PROJECT_DIR_TEMP:=$(patsubst %/makefile,%, $(abspath $(lastword ${MAKEFILE_LIST})))
 #! DONT EDIT ABOVE THIS
 
 # exe name
 PROJECT:=main
 # the directory in which all .o and .d files will be made
 OBJ_O_DIR:=bin
-# assumes that source directories are the same as include
-SRCDIRS:=.
-# the include flags for compilation
-INCLUDES:=-I /VSCodeFolder/Libraries/SFML-2.6.1/include -D SFML_STATIC -I /VSCodeFolder/Libraries/TGUI-1.1/include -I $(project_dir)
+# the include flags for compilation by default includes the project directory and include directory
+INCLUDE_DIRS=/VSCodeFolder/Libraries/SFML-2.6.1/include /VSCodeFolder/Libraries/TGUI-1.2/include
+# extra include flags
+INCLUDE_FLAGS=-D SFML_STATIC
 # the paths to libs for linking
-LIBS=-L /VSCodeFolder/Libraries/SFML-2.6.1/lib -L /VSCodeFolder/Libraries/TGUI-1.1/lib -L $(project_dir)
+LIB_DIRS=/VSCodeFolder/Libraries/SFML-2.6.1/lib /VSCodeFolder/Libraries/TGUI-1.2/lib
+# source files directory (the project directory is automatically added)
+SRC:=src
+# the directory for lib files that are made with "make lib"
+# this should be the full path
+LIB_DIR:=${PROJECT_DIR_TEMP}/lib
+# the directory where all the source files that you want in the lib are
+LIB_SOURCE:=src
+
+# compiler command
+CC:=g++
 # linker flags for compilation
 # add "-mwindows" to disable the terminal
-LINKERFLAGS=$(LIBS) \
-			-ltgui-s -lsfml-graphics-s -lsfml-window-s \
+LINKERFLAGS:=-ltgui-s -lsfml-graphics-s -lsfml-window-s \
 			-lsfml-system-s -lsfml-audio-s -lsfml-network-s \
 			-lws2_32 -lflac -lvorbisenc -lvorbisfile -lvorbis \
 			-logg -lopenal32 -lopengl32 -lwinmm -lgdi32 -lfreetype \
-			-lstdc++ \
-			# -mwindows
-
-# the directory for lib files
-LIB_DIR:=lib
+			-lstdc++
+# flags to generate dependencies for all .o files
+DEPFLAGS:=-MP -MD
+# any compiler options
+# put -Werror for warnings to be treated as errors
+# use -Wextra -Wall every so often to find possible bugs
+COMPILE_OPTIONS:=-std=c++20 -g -static
 
 #! DONT EDIT ANYTHING FROM HERE DOWN
-# any of the dirs for source files
-# DIRS:=dir /AD /B /S	
-# findSubDirs=$(shell ${DIRS} $1)
+
+ifeq ($(OS),Windows_NT)
+	EXE:=.exe
+	FIXPATH=$(subst /,\,$1)
+	PROJECT_DIR:=$(call FIXPATH,${PROJECT_DIR_TEMP})
+	SOURCEDIRS:=$(call FIXPATH,$(shell dir ${SRC} /s /b /ad)) $(call FIXPATH,${PROJECT_DIR}/${SRC}) $(call FIXPATH,${PROJECT_DIR})
+	LIBSOURCEDIRS:=$(call FIXPATH,$(shell dir "${LIB_SOURCE}" /s /b /ad)) $(call FIXPATH,${PROJECT_DIR}/${LIB_SOURCE})
+	RM=del /q /f
+	RMDIR=rmdir /S /Q
+	MD:=mkdir
+# if you are using wsl or something which requires a more exact path add it here (this path will be added in front of all lib_dirs and include_dirs)
+	DRIVEPATH:=
+else
+	EXE:=
+	FIXPATH=$(subst \,/,$1)
+	PROJECT_DIR:=$(call FIXPATH,${PROJECT_DIR_TEMP})
+	SOURCEDIRS:=$(shell find "$$PWD" -type d -path "*${PROJECT_DIR}/${SRC}*") ${PROJECT_DIR}
+	RM=rm -r --preserve-root
+	RMDIR=rm -r --preserve-root
+	MD:=mkdir -p
+# if you are using wsl or something which requires a more exact path add it here (this path will be added in front of all lib_dirs and include_dirs)
+	DRIVEPATH:=/mnt/v
+endif
+
+INCLUDES:=$(addprefix -I ${DRIVEPATH},$(call FIXPATH,${INCLUDE_DIRS})) ${INCLUDE_FLAGS} -I ${PROJECT_DIR} -I ${PROJECT_DIR}/include
+LIBS:=$(addprefix -L ${DRIVEPATH},$(call FIXPATH,${LIB_DIRS}))
 
 # all .cpp file paths
-SRC:=$(foreach D,$(SRCDIRS),$(wildcard $(D)/*.cpp))
+SRC:=$(foreach D,${SOURCEDIRS},$(wildcard ${D}/*.cpp))
+LIBSRC:=$(foreach D,${LIBSOURCEDIRS},$(wildcard ${D}/*.cpp))
 # Create an object file of every cpp file
-OBJECTS=$(addprefix $(OBJ_O_DIR)/,$(patsubst %.cpp,%.o,$(SRC)))
+OBJECTS:=$(patsubst ${PROJECT_DIR}%,${PROJECT_DIR}/${OBJ_O_DIR}%,$(call FIXPATH,$(patsubst %.cpp,%.o,${SRC})))
+LIBOBJECTS:=$(patsubst ${PROJECT_DIR}%,${PROJECT_DIR}/${OBJ_O_DIR}%,$(call FIXPATH,$(patsubst %.cpp,%.o,${LIBSRC})))
 # Creating dependency files
-DEPFILES=$(patsubst %.o,%.d,$(OBJECTS))
-
-OUTPATHS:=$(foreach d,$(filter-out ./,$(SRCDIRS)),$(OBJ_O_DIR)/$(d)/)
+DEPFILES=$(patsubst %.o,%.d,${OBJECTS})
+# All bin directories
+BIN_DIRS=$(foreach dir,$(call FIXPATH,$(SOURCEDIRS)),$(patsubst $(call FIXPATH,$(PROJECT_DIR)%),$(call FIXPATH,$(PROJECT_DIR)/$(OBJ_O_DIR)%),$(dir)))
 
 # so there is no file that gets mistaked with the tasks listed
-.PHONY = all info clean libs dirs
+.PHONY = all info clean lib run
 
-# compiler to use
-CC=g++
+all: ${BIN_DIRS} ${PROJECT}
 
-# static (remove -static for it to no longer be static)
-STATIC_BUILD=-static
-# debugging build (remove -g for the compiled project to not be a debugging build)
-DEBUG_BUILD=-g
-# flags to generate dependencies for all .o files
-DEPFLAGS=-MP -MD
-# any compiler options
-COMPILE_OPTIONS=-std=c++20 $(DEBUG_BUILD) $(STATIC_BUILD) $(INCLUDES) $(DEPFLAGS)
+# main build
+${PROJECT}: ${OBJECTS}
+	${CC} ${COMPILE_OPTIONS} ${INCLUDES} -o ${@} ${^} ${LIBS} ${LINKERFLAGS}
 
-all: $(OUTPATHS) $(PROJECT)
+# build objects for the main build
+${PROJECT_DIR}/${OBJ_O_DIR}%.o:${PROJECT_DIR}%.cpp
+	${CC} ${COMPILE_OPTIONS} ${INCLUDES} ${DEPFLAGS} -c -o ${@} ${<}
 
-# try interlacing the objects and header files
-$(PROJECT): $(OBJECTS)
-	$(CC) -o $@ $(STATIC_BUILD) $^ $(DEBUG_BUILD) $(LINKERFLAGS)
-
-$(OBJ_O_DIR)/%.o:%.cpp
-	$(CC) $(COMPILE_OPTIONS) -c -o $@ $<
-
-# include the dependencies
--include $(DEPFILES)
-
-$(OBJ_O_DIR)/%/:
-	$(call makeDir,$(patsubst %/,%,$@))
-
-# shell command for removing a dir
-RMDIR:=rmdir /s /q
-# shell command for removing a file
-RMFILE:=del /q /f
-
-clean:
-	$(shell ${RMFILE} $(PROJECT).exe)
-	$(shell ${RMDIR} $(OBJ_O_DIR))
-	$(call makeBinDir)
-
-# the shell command for making a dir
-MKDIR_P:=mkdir
-# makes a dir for the given path
-makeDir=$(shell ${MKDIR_P} $(subst /,\,$1))
-# make bin directories 
-makeBinDir=$(foreach d,$(filter-out ./,$(SRCDIRS)),$(call makeDir,$(patsubst $(OBJ_O_DIR)/%/,$(OBJ_O_DIR)/%,$(OBJ_O_DIR)/$(d))))
-
-libs: $(patsubst $(OBJ_O_DIR)/%,$(LIB_DIR)/%,$(OUTPATHS)) $(patsubst $(OBJ_O_DIR)/%.o,$(LIB_DIR)/%.a,$(OBJECTS))
+# build the lib with the same compile options
+lib: ${BIN_DIRS} ${LIB_DIR} ${LIBOBJECTS}
+	ar rcs $(call FIXPATH,${PROJECT_DIR}/${LIB_DIR}/${PROJECT}.a) ${LIBOBJECTS}
 	@echo Libs created
 
-$(LIB_DIR)/%.a:$(OBJ_O_DIR)/%.o
-	ar rcs $@ $^
+# include the dependencies
+-include ${DEPFILES}
 
-$(LIB_DIR)/%/:
-	$(call makeDir,$(patsubst %/,%,$@))
+# on linux it confuses the create directory target with dependency files
+$(call FIXPATH,${PROJECT_DIR}/${OBJ_O_DIR})%.d:
+	@echo -n ""
 
-dirs: src/ include/ lib/
-	@echo Created Project Directories
+$(call FIXPATH,${PROJECT_DIR}/${OBJ_O_DIR})%:
+	$(call makeDir,$(call FIXPATH,${@}))
 
-%/:
-	$(call makeDir,$(patsubst %/,%,$@))
+${LIB_DIR}:
+	$(call makeDir,$(call FIXPATH,${PROJECT_DIR}/${LIB_DIR}))
+
+clean:
+	$(shell ${RMDIR} ${OBJ_O_DIR} $(call FIXPATH,${PROJECT_DIR}/${LIB_DIR}))
+	$(shell ${RM} ${PROJECT}${EXE})
+
+# builds and runs the program
+run: ${BIN_DIRS} ${PROJECT}
+	./${PROJECT}${EXE}
+
+# makes a dir for the given path
+makeDir=$(shell ${MD} $(call FIXPATH,$1))
+# make bin directories 
+# makeBinDir=$(foreach dir,${BIN_DIRS},$(call makeDir,${dir}))
 
 info:
-	@echo Project Name: $(project_name)
-	@echo Makefile Dir: $(mkfile_path)
-	@echo Project Dir: $(project_dir)
-	@echo Compiler: $(CC)
-	@echo Object Folder: $(OBJ_O_DIR)
-	@echo Output Paths: $(OUTPATHS)
-	@echo Source Files: $(SRC)
-	@echo Linker Flags: $(LINKERFLAGS)
-	@echo Libraries: $(LIBS)
-	@echo Includes: $(INCLUDES)
-
-help:
-	@echo to make the project call "make"
-	@echo to make static libs for each src file call "make libs"
-	@echo to make project directories call "make dirs"
-	@echo to get project info call "make info"
-	@echo to clean .o and .exe files call "make clean"
+	@echo Project Directory: ${PROJECT_DIR}
+	@echo Source Directories: ${SOURCEDIRS} 
+	@echo Lib Source Directories: ${LIBSOURCEDIRS} 
+	@echo temp: ${COMPILE_OPTIONS} ${INCLUDES}
+	@echo Object Files: ${OBJECTS}
+	@echo Dependencies: ${DEPFILES}
